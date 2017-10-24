17dc323d62502161ce29896282700904f3074ca6
Antero Campos Gandra <up201607926@fe.up.pt>
2017-10-24 12:39:01 +0100

    SQL files
    List- por implementar

---
+++ SQL/list.sql
@@ -0,0 +1,15 @@
+insert into list (title, iditem) values ("compras", "1");
+insert into list (title, iditem) values ("cenas", "2");
+insert into list (title, iditem) values ("documentos", "3");
+
+insert into items (iditem,item)values ("1","cenouras");
+insert into items (iditem,item)values ("1","batatas");
+insert into items (iditem,item)values ("1","comida");
+
+insert into items (iditem,item)values ("2","exemplo");
+insert into items (iditem,item)values ("2","genérico");
+insert into items (iditem,item)values ("2","etc");
+
+insert into items (iditem,item)values ("3","cv");
+insert into items (iditem,item)values ("3","presentation");
+insert into items (iditem,item)values ("3","pdf");
---
+++ SQL/users.sql
@@ -0,0 +1,10 @@
+insert into user (username, name, birthday, email, countryname) values ("rogmat7", "Rogerio Matias","10-02-1989","rogmatias7@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("jofermatos", "Joana Matos","27-07-1991","joanaferreiramatos@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Ricugh", "Richard Hugh","12-03-1987","Rich_hugh87@hotmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Allen", "Ray Allen","30-12-1988","raymfallen@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Bruninho", "Bruno Santos","11-07-1989","brunosantos656@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Katie", "Kate Midleton","15-09-1985","kateofmidletonsfamily@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Augustinus", "Agostino Constanzo","12-05-1986","augconstanzo@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Sean9", "Sean Conner","14-01-1990","seancconner1990@gmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Didier", "Didier Lloris","18-01-1990","didiersamlloris@hotmail.com","Portugal");
+insert into user (username, name, birthday, email, countryname) values ("Alberto", "Alberto Garcia","30-03-1981","albertoogarcia99@hotmail.com","Portugal");
