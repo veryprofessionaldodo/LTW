a25b92810cbbf5b22b5c17939169575117d8dacb
Vicente Espinha <up201503764@fe.up.pt>
2017-11-14 12:45:15 +0000

    :1234:

--- code/register.php
+++ code/register.php
@@ -1,6 +1,9 @@
 <?php
-  $db = new PDO('sqlite:users.db');
-  $stmt = $db->prepare('INSERT INTO user (username, email, password) VALUES (?, ?, ?)');
-  $stmt->execute(array($username, $email, $password));
+  //$db = new PDO('sqlite:users.db');
+  //$stmt = $db->prepare('INSERT INTO user (username, email, password) VALUES (?, ?, ?)');
+  //$stmt->execute(array($Name, $Email, $Password));
+  echo $_POST["name"];
+  echo $_POST["Email"];
+  echo $_POST["Password"];
 
 ?>
