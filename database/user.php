4cbb19882331990bf25440d35323ca560443353b
Antero Campos Gandra <up201607926@fe.up.pt>
2017-12-04 16:12:40 +0000

    more functions

--- database/user.php
+++ database/user.php
@@ -5,4 +5,13 @@
     $stmt->execute(array($username, sha1($password)));
     return $stmt->fetch() !== false;
   }
+
+  function user_Get($Name){
+    global $dbh;
+    $stmt = $dbh->prepare('SELECT * FROM user WHERE Name = ?');
+    $stmt->execute(array($Name));
+    return $stmt->fetch();
+  }
+
+
 ?>
