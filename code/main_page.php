14dc73ba604cf654818a1317acfd6e9ba5b641d1
Pedro Daniel Reis <up201506046@fe.up.pt>
2017-12-04 17:17:39 +0000

    Improved main page

--- code/main_page.php
+++ code/main_page.php
@@ -4,7 +4,7 @@
 	<title>List Checkers</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="main_page.css" rel="stylesheet">
+	<link href="css/main_page.css" rel="stylesheet">
 </head>
 <body>
 	<div id="sidebar">
