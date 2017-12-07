9a2b11f3f59de128c7924ab2963826fa9d49c35c
Antero Campos Gandra <up201607926@fe.up.pt>
2017-12-07 13:19:03 +0000

    sql final

--- database/users.sql
+++ database/users.sql
@@ -1,10 +1,36 @@
 .nullvalue NULL
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS list;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS items;
 
 CREATE TABLE user (
   Name TEXT NOT NULL,
   Email TEXT PRIMARY KEY,
   Password TEXT NOT NULL
+);
+
+CREATE TABLE list(
+  Id INTEGER NOT NULL PRIMARY KEY,
+  Title TEXT NOT NULL,
+  Data DATE NOT NULL,
+  Category TEXT NOT NULL,
+  Email TEXT NOT NULL,
+  FOREIGN KEY (Category) REFERENCES category(Title),
+  FOREIGN KEY (Email) REFERENCES user(Email)
+);
+
+CREATE TABLE items(
+  Id INTEGER NOT NULL,
+  Content TEXT NOT NULL,
+  Data DATE NOT NULL,
+  Completed INTEGER DEFAULT 0,
+  FOREIGN KEY (Id) REFERENCES list(Id)
+);
+
+CREATE TABLE category(
+  Title TEXT PRIMARY KEY,
+  Color TEXT NOT NULL
 );
 
 insert into user (Name, Email, Password) values ("rogmat7", "rogmatias7@gmail.com","Portugal");
@@ -18,3 +44,9 @@ insert into user (Name, Email, Password) values ("Augustinus","augconstanzo@gmai
 insert into user (Name, Email, Password) values ("Sean9", "seancconner1990@gmail.com","Portugal");
 insert into user (Name, Email, Password) values ("Didier", "didiersamlloris@hotmail.com","Portugal");
 insert into user (Name, Email, Password) values ("Alberto","albertoogarcia99@hotmail.com","Portugal");
+
+insert into category (Title,Color) values ("LTW","Blue");
+
+insert into list (Id, Title, Data, Category, Email) values (1, "LTW", "2017-10-10", "LTW", "rogmatias7@gmail.com");
+
+insert into items (Id, Content, Data, Completed) values (1, "LTW","2017-10-10",1);
--- register.php
+++ register.php
@@ -3,12 +3,12 @@
 function register($username,$email,$password){
 	include_once('connection.php');
 
-  	global $db;
+  	global $dbh;
   	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
      	echo "Invalid email format";
      	return false; 
    	}
-	$stmt = $db->prepare('INSERT INTO user (Name, Email, Password) VALUES ('$username','$email','$password')');
+	$stmt = $dbh->prepare('INSERT INTO user (Name, Email, Password) VALUES ('$username','$email','$password')');
   	$stmt->execute(array($username, $email, $password));
 }
 
